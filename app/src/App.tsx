c632ad2254e73be1c87b014b33cc4c5bd999595a
AlexMcDermott <alexjmcd2202@gmail.com>
2024-03-15 00:16:51 +1000

    Basic chat form

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, {
+  ChangeEventHandler,
+  FormEvent,
+  FormEventHandler,
+  useState,
+} from "react";
+
+type Message = {
+  timestamp: Date;
+  text: string;
+  author: string;
+};
 
 function App() {
-  return <>hello world</>;
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [input, setInput] = useState("");
+
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setInput(e.target.value);
+  };
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    if (!input) return;
+
+    const newMessage: Message = {
+      timestamp: new Date(),
+      text: input,
+      author: "client",
+    };
+
+    setMessages([...messages, newMessage]);
+    setInput("");
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input value={input} onChange={onChange} />
+      </form>
+
+      {messages.map(({ timestamp, text, author }) => (
+        <div key={Number(timestamp)}>
+          {author}: {text}
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default App;
