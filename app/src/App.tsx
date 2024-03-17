86c24358c02474892994111128c1b3a0256e395d
AlexMcDermott <alexjmcd2202@gmail.com>
2024-03-17 22:35:28 +1000

    working socket chat server

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,3 @@
-import { edenTreaty } from "@elysiajs/eden";
-import { EdenTreaty } from "@elysiajs/eden/treaty";
 import QRCode from "qrcode";
 import {
   ChangeEventHandler,
@@ -8,52 +6,52 @@ import {
   useRef,
   useState,
 } from "react";
-import { App, Message } from "../../server/src";
-
-type Socket = ReturnType<EdenTreaty.Create<App>["ws"]["subscribe"]>;
+import { Socket, io } from "socket.io-client";
+import type {
+  ClientToServerEvents,
+  ServerToClientEvents,
+  SocketData,
+} from "../../server/src";
 
 function App() {
-  const [messages, setMessages] = useState<Message[]>([]);
+  const [messages, setMessages] = useState<SocketData[]>([]);
   const [input, setInput] = useState("");
   const [qrCode, setQrCode] = useState<string>();
 
-  const ws = useRef<Socket>();
+  const socket = useRef<Socket<ServerToClientEvents, ClientToServerEvents>>();
 
   useEffect(() => {
-    const onDeviceMotion = (event: DeviceMotionEvent) => {
-      alert("motion");
-      alert(JSON.stringify(event.acceleration));
-      ws.current?.send({
-        timestamp: Number(new Date()),
-        text: JSON.stringify(event.acceleration),
-        author: "client",
-      });
-    };
+    // const onDeviceMotion = (event: DeviceMotionEvent) => {
+    //   alert("motion");
+    //   alert(JSON.stringify(event.acceleration));
+    //   ws.current?.send({
+    //     timestamp: Number(new Date()),
+    //     text: JSON.stringify(event.acceleration),
+    //     author: "client",
+    //   });
+    // };
 
-    window.addEventListener("devicemotion", onDeviceMotion);
+    // window.addEventListener("devicemotion", onDeviceMotion);
 
     (async () => {
       const url = `http://${location.host}/`;
       setQrCode(await QRCode.toDataURL(url));
     })();
 
-    const client = edenTreaty<App>(`${location.href}:8080`);
+    socket.current = io(`${location.hostname}:${8080}`);
 
-    console.log(location);
-    ws.current = client.ws.subscribe();
+    socket.current.on("catchup", (messages) => {
+      setMessages(messages);
+    });
 
-    ws.current.on("error", (e) => console.log(e));
-
-    ws.current?.subscribe(({ data: newMessage }) => {
+    socket.current.on("message", (newMessage) => {
       setMessages((oldMessages) => [...oldMessages, newMessage]);
     });
 
     return () => {
-      if (ws.current?.ws.readyState === ws.current?.ws.OPEN) {
-        ws.current?.close();
-      }
+      socket.current?.close();
 
-      window.removeEventListener("devicemotion", onDeviceMotion);
+      // window.removeEventListener("devicemotion", onDeviceMotion);
     };
   }, []);
 
@@ -65,8 +63,7 @@ function App() {
     e.preventDefault();
     if (!input) return;
 
-    ws.current?.send({
-      timestamp: Number(new Date()),
+    socket.current?.emit("message", {
       text: input,
       author: "client",
     });
@@ -74,27 +71,29 @@ function App() {
     setInput("");
   };
 
-  const onClick = async () => {
-    try {
-      DeviceOrientationEvent.requestPermission();
-    } catch {
-      console.log("yarn't on iOS son");
-    }
-  };
+  // const onClick = async () => {
+  //   try {
+  //     DeviceOrientationEvent.requestPermission();
+  //   } catch {
+  //     console.log("yarn't on iOS son");
+  //   }
+  // };
 
   return (
     <>
-      <button onClick={onClick}>Start</button>
+      {/* <button onClick={onClick}>Start</button> */}
 
       <form onSubmit={onSubmit}>
         <input value={input} onChange={onChange} type="text" />
       </form>
 
-      {messages.map(({ timestamp, text, author }) => (
-        <div key={timestamp}>
-          {author}: {text}
-        </div>
-      ))}
+      {messages.map(({ timestamp, text, author }) => {
+        return (
+          <div key={timestamp}>
+            {author}: {text}
+          </div>
+        );
+      })}
 
       {qrCode && <img src={qrCode}></img>}
     </>
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,37 +1,68 @@
-import { Elysia, Static, t } from "elysia";
+import cors from "cors";
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-const messageScheme = t.Object({
-  timestamp: t.Number(),
-  text: t.String(),
-  author: t.String(),
+export type ServerToClientEvents = {
+  message: (message: SocketData) => void;
+  catchup: (messages: SocketData[]) => void;
+};
+
+export type ClientToServerEvents = {
+  message: (message: Omit<SocketData, "timestamp">) => void;
+};
+
+type InterServerEvents = {};
+
+export type SocketData = {
+  timestamp: string;
+  text: string;
+  author: string;
+};
+
+const app = express();
+app.use(cors());
+
+const server = createServer(app);
+const io = new Server<
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents,
+  SocketData
+>(server, {
+  cors: {
+    origin: true,
+  },
 });
 
-export type Message = Static<typeof messageScheme>;
+const messages: SocketData[] = [];
 
-const app = new Elysia()
-  .ws("/ws", {
-    body: messageScheme,
-    response: messageScheme,
-    message: (ws, message) => {
-      console.log(JSON.stringify(message));
-      ws.send(message);
-      // ws.send({
-      //   timestamp: Number(Date.now()),
-      //   text: message.text.toLocaleUpperCase(),
-      //   author: "server",
-      // });
-    },
-    open: (ws) => {
-      console.log(`client connected: ${ws.id}`);
-    },
-    close: (ws) => {
-      console.log(`client disconnected: ${ws.id}`);
-    },
-  })
-  .listen(8080);
+const logActiveConnections = async (io: Server<any>, ...args: any[]) => {
+  const sockets = await io.fetchSockets();
+  console.log(`active connections: ${sockets.length}`, ...args);
+};
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+io.on("connection", async (socket) => {
+  logActiveConnections(io, `(${socket.id} joined)`);
 
-export type App = typeof app;
+  socket.emit("catchup", messages);
+
+  socket.on("message", (data) => {
+    console.log("got da msg:", data.text);
+    const message: SocketData = {
+      ...data,
+      timestamp: new Date().toISOString(),
+    };
+
+    messages.push(message);
+    io.emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    logActiveConnections(io, `(${socket.id} left)`);
+  });
+});
+
+server.listen(8080, () => {
+  console.log("she up bb ⬆️");
+});
